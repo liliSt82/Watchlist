eb2026fc222a55dc2c4b98cdb3b893536aa39c37
Lili <honolulubg@abv.bg>
2022-11-28 23:21:30 +0200

    UploadWatchlist

---
+++ Watchlist/Services/MovieService.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Watchlist.Contracts;
+using Watchlist.Data;
+using Watchlist.Data.Models;
+using Watchlist.Models;
+
+namespace Watchlist.Services
+{
+    public class MovieService : IMovieService
+    {
+        private readonly WatchlistDbContext context;
+
+        public MovieService(WatchlistDbContext _context)
+        {
+            context = _context;
+        }
+
+        public async Task AddMovieAsync(AddMovieViewModel model)
+        {
+            var entity = new Movie()
+            {
+                Director = model.Director,
+                Title = model.Title,
+                GenreId = model.GenreId,
+                ImageUrl = model.ImageUrl,
+                Rating = model.Rating,
+            };
+
+            await context.Movies.AddAsync(entity); 
+            await context.SaveChangesAsync();
+        }
+
+        public async Task AddMovieToCollectionAsync(int movieId, string userId)
+        {
+            var user = await context.Users
+                .Where(u => u.Id == userId)
+                .Include(u => u.UsersMovies)
+                .FirstOrDefaultAsync();
+
+            if (user==null)
+            {
+                throw new ArgumentException("Invalid user Id");
+            }
+
+            var movie=await context.Movies.FirstOrDefaultAsync(u => u.Id == movieId);
+
+            if (movie==null)
+            {
+                throw new ArgumentException("Invalid movie Id");
+            }
+
+            if (!user.UsersMovies.Any(m=>m.MovieId==movieId))
+            {
+                user.UsersMovies.Add(new UserMovie()
+                {
+                    MovieId = movie.Id,
+                    UserId = user.Id,
+                    Movie = movie,
+                    User = user
+                });
+
+                await context.SaveChangesAsync();
+            }
+            
+        }
+
+        public async Task<IEnumerable<MovieViewModel>> GetAllAsync()
+        {
+            var entities = await context.Movies
+                .Include(x => x.Genre)
+                .ToListAsync();
+
+            return entities.Select(m => new MovieViewModel()
+            {
+                Id = m.Id,
+                Title = m.Title,
+                Director = m.Director,
+                Genre = m.Genre?.Name,
+                ImageUrl = m.ImageUrl,
+                Rating = m.Rating
+            });
+        }
+
+        public async Task<IEnumerable<Genre>> GetGenresAsync()
+        {
+            return await context.Genres.ToListAsync();
+        }
+
+        public async Task<IEnumerable<MovieViewModel>> GetWatchedAsync(string userId)
+        {
+            var user = await context.Users
+                .Where(u => u.Id == userId)
+                .Include(u=>u.UsersMovies)
+                .ThenInclude(um=>um.Movie)
+                .ThenInclude(m=>m.Genre)
+                .FirstOrDefaultAsync();
+
+            if (user == null)
+            {
+                throw new ArgumentException("Invalid user Id");
+            }
+
+            return user.UsersMovies
+                .Select(m => new MovieViewModel()
+                {
+                    Director = m.Movie.Director,
+                    Genre = m.Movie.Genre?.Name,
+                    Id = m.Movie.Id,
+                    ImageUrl = m.Movie.ImageUrl,
+                    Title = m.Movie.Title,
+                    Rating = m.Movie.Rating
+                });
+        }
+
+        public async Task RemoveMovieFromCollectionAsync(int movieId, string userId)
+        {
+            var user = await context.Users
+                .Where(u => u.Id == userId)
+                .Include(u => u.UsersMovies)
+                .FirstOrDefaultAsync();
+
+            if (user == null)
+            {
+                throw new ArgumentException("Invalid user Id");
+            }
+
+            var movie=user.UsersMovies.FirstOrDefault(m=>m.MovieId==movieId); 
+            
+
+            if (movie!=null)
+            {
+                user.UsersMovies.Remove(movie);
+            }
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
